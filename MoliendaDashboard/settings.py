2c771561117639306f92766f79805750efcf9a1f
Chainsmoker <75154124+Chainsmoker@users.noreply.github.com>
2023-03-14 12:33:58 -0500

    Update settings.py

--- MoliendaDashboard/settings.py
+++ MoliendaDashboard/settings.py
@@ -26,10 +26,9 @@ SECRET_KEY = 'django-insecure-1ia--hbp$jm^pl8(ze=_0m%hsekv%t)b8z1j!h1cg=5=z+)4#*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-if not DEBUG:
-    CSRF_TRUSTED_ORIGINS = ["https://web-production-bd40.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ['*','https://*.127.0.0.1']
 
-ALLOWED_HOSTS = ['https://web-production-bd40.up.railway.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
